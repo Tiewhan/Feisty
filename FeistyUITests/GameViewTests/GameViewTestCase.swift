ae8023a503fa6bbbdcedba8744df40e4727f4a61
Tiewhan Smith <TSmith@jhb.dvt.co.za>
2020-03-17 08:29:41 +0200

    Add More Login UI Test Cases

---
+++ FeistyUITests/GameViewTests/GameViewTestCase.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewTestCase.swift
+//  FeistyUITests
+//
+//  Created by Tiewhan Smith on 2020/03/16.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import XCTest
+
+class GameViewTestCase: XCTestCase {
+  
+  var application: XCUIApplication!
+  
+  override func setUp() {
+    
+    continueAfterFailure = false
+    
+    application = XCUIApplication()
+  }
+  
+  func loginUserWithDetails() {
+    
+    application.textFields["Username"].tap()
+    
+    UIPasteboard.general.string = "tiewhan"
+    application.textFields["Username"].doubleTap()
+    application.menuItems["Paste"].tap()
+    
+    application.textFields["Password"].doubleTap()
+    UIPasteboard.general.string = "cat3"
+    application.textFields["Password"].tap()
+    application.menuItems["Paste"].tap()
+    
+    application.buttons["Login"].tap()
+    
+    application.alerts["Login Message"].buttons["Dismiss"].tap()
+    
+    sleep(1)
+    
+  }
+  
+}
---
+++ FeistyUITests/GameViewTests/GameViewUITests.swift
@@ -0,0 +1,56 @@
+//
+//  GameViewUITests.swift
+//  FeistyUITests
+//
+//  Created by Tiewhan Smith on 2020/03/16.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import XCTest
+@testable import Feisty
+@testable import CommonFiles
+
+class GameViewUITests: GameViewTestCase {
+
+  func testLoadGames() {
+    
+    application.launch()
+    
+    loginUserWithDetails()
+    
+    let listItem = application.staticTexts["test2"]
+    let exsists = NSPredicate(format: "exists == 1")
+    
+    expectation(for: exsists, evaluatedWith: listItem, handler: nil)
+    waitForExpectations(timeout: 20, handler: nil)
+    
+  }
+  
+  func testLoadGameDetailsThenBack() {
+    
+    application.launch()
+    
+    loginUserWithDetails()
+    
+    let gameNameLabel = application.staticTexts["test2"]
+    let exists = NSPredicate(format: "exists == 1")
+    
+    expectation(for: exists, evaluatedWith: gameNameLabel, handler: nil)
+    waitForExpectations(timeout: 20) { [weak self] (error) in
+      
+      guard error != nil else {
+        return
+      }
+      
+      self?.application.staticTexts["test2"].tap()
+      sleep(1)
+      XCTAssert(self?.application.staticTexts["test2"].exists ?? false)
+      
+      self?.application.navigationBars.buttons["Games"].tap()
+      XCTAssert(self?.application.staticTexts["Games In Store"].exists ?? false)
+      
+    }
+    
+  }
+  
+}
