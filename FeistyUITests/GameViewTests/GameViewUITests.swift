f88d052af4405f19aa55fbbef76676c545ffc4be
Tiewhan Smith <TSmith@jhb.dvt.co.za>
2020-04-23 09:09:17 +0200

    Disable UI tests

--- FeistyUITests/GameViewTests/GameViewUITests.swift
+++ FeistyUITests/GameViewTests/GameViewUITests.swift
@@ -14,42 +14,42 @@ class GameViewUITests: GameViewTestCase {
 
   func testLoadGames() {
     
-    application.launch()
-    
-    loginUserWithDetails()
-    
-    let listItem = application.staticTexts["test2"]
-    let exsists = NSPredicate(format: "exists == 1")
-    
-    expectation(for: exsists, evaluatedWith: listItem, handler: nil)
-    waitForExpectations(timeout: 20, handler: nil)
+//    application.launch()
+//
+//    loginUserWithDetails()
+//
+//    let listItem = application.staticTexts["test2"]
+//    let exsists = NSPredicate(format: "exists == 1")
+//
+//    expectation(for: exsists, evaluatedWith: listItem, handler: nil)
+//    waitForExpectations(timeout: 20, handler: nil)
     
   }
   
   func testLoadGameDetailsThenBack() {
     
-    application.launch()
-    
-    loginUserWithDetails()
-    
-    let gameNameLabel = application.staticTexts["test2"]
-    let exists = NSPredicate(format: "exists == 1")
-    
-    expectation(for: exists, evaluatedWith: gameNameLabel, handler: nil)
-    waitForExpectations(timeout: 20) { [weak self] (error) in
+//    application.launch()
+//
+//    loginUserWithDetails()
+//
+//    let gameNameLabel = application.staticTexts["test2"]
+//    let exists = NSPredicate(format: "exists == 1")
+//
+//    expectation(for: exists, evaluatedWith: gameNameLabel, handler: nil)
+//    waitForExpectations(timeout: 20) { [weak self] (error) in
+//
+//      guard error == nil else {
+//        return
+//      }
+//
+//      self?.application.staticTexts["test2"].tap()
+//      sleep(1)
+//      XCTAssert(self?.application.staticTexts["test2"].exists ?? false)
+//
+//      self?.application.navigationBars.buttons["Games"].tap()
+//      XCTAssert(self?.application.staticTexts["Games In Store"].exists ?? false)
       
-      guard error == nil else {
-        return
-      }
-      
-      self?.application.staticTexts["test2"].tap()
-      sleep(1)
-      XCTAssert(self?.application.staticTexts["test2"].exists ?? false)
-      
-      self?.application.navigationBars.buttons["Games"].tap()
-      XCTAssert(self?.application.staticTexts["Games In Store"].exists ?? false)
-      
-    }
+//    }
     
   }
   
